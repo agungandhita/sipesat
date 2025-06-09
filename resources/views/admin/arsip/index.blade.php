13537ac2cefcbad2a1a617f07a774838c2291776
agungandhita <agungandhita@gmail.com>
2025-06-09 07:55:44 +0700

    fix(views): correct storage path for incoming mail files

--- resources/views/admin/arsip/index.blade.php
+++ resources/views/admin/arsip/index.blade.php
@@ -50,7 +50,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{{ $item->asal_surat }}
                                     </td>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm">
-                                        <a href="{{ $item->jenis_surat == 'masuk' ? Storage::url('public/surat/' . $item->file_surat) : Storage::url('public/surat_keluar/' . $item->file_surat) }}"
+                                        <a href="{{ $item->jenis_surat == 'masuk' ? Storage::url(  $item->file_surat) : Storage::url('public/surat_keluar/' . $item->file_surat) }}"
                                             target="_blank"
                                             class="inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none"
